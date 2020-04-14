4b136cb0c3082f89b3f9e54e0d6b68266e1b879b
Rachel <rachel@Rachels-MacBook-Pro.local>
2020-04-13 23:33:14 -0600

    configured webpack-dev-server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './index.css'
 
-const App = () => (
-  <div>hello webpack</div>
-)
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const [values, setValues] = useState([])
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+    setValues(values.concat(counter))
+  }
+
+  return (
+    <div className="container">
+      hello webpack {counter} clicks
+      <button onClick={handleClick}>
+        press
+      </button>
+    </div>
+  )
+}
 
 export default App
--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,24 @@ const config = {
     path: path.resolve(__dirname, 'build'),
     filename: 'main.js'
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+  },
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
         query: {
-          presets: ['@babel/preset-react'],
+          presets: ['@babel/preset-env', '@babel/preset-react'],
         },
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
       },
     ],
   },
